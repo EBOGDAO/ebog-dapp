bcbff8bff573d652b944229451e48b135da1d302
Sahil Mehta <samehta91@gmail.com>
2021-07-26 22:32:47 -0700

    Deploy contract to rinkeby testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,22 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
-
-// The next line is part of the sample project, you don't need it in your
-// project. It imports a Hardhat task definition, that can be used for
-// testing the frontend.
 require("./tasks/faucet");
 
 module.exports = {
   solidity: "0.8.0",
+  paths: {
+    artifacts: "./frontend/src/artifacts"
+  },
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
+      forking: {
+        url: "https://eth-rinkeby.alchemyapi.io/v2/" + `${process.env.ALCHEMY_API_KEY}`
+      }
+    },
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/" + `${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.RINKEBY_PRIVATE_KEY}`]
     }
   }
 };
