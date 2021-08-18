0684bf209cdfc4ded95c08b7f0d938d6f7f0d55a
Sahil Mehta <samehta91@gmail.com>
2021-08-17 17:11:43 -0700

    Add tests for opting in and out of DAO

--- frontend/src/components/NoWalletDetected.js
+++ frontend/src/components/NoWalletDetected.js
@@ -3,8 +3,8 @@ import React from "react";
 export function NoWalletDetected() {
   return (
     <div className="container">
-      <div className="row justify-content-md-center">
-        <div className="col-6 p-4 text-center">
+      <div className="row justify-content-center">
+        <div className="col-12 mt-5 p-4 text-center">
           <p>
             No Ethereum wallet was detected. <br />
             Please install{" "}
